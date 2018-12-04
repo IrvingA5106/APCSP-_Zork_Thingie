30388ab4f1f78327732b232899ae6c2f3bd8ac62
MggMuggins <mggmugginsmc@gmail.com>
2018-12-04 14:21:02 -0600

    Fix basement; Start Game works now

--- mainZorkCode.py
+++ mainZorkCode.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
-import random
+
+from basement import basement
+from player import Player
 
 playerItems = []
 enteredRooms = []
@@ -7,5 +9,9 @@ openedDoors = []
 
 def startGame():
     print('Welcome to the mysterious mansion')
-    raw_input('Press enter to start the game. ')
-    room1()
+    # raw_input('Press enter to start the game. ')
+    player = Player()
+    
+    basement(player)
+
+startGame()
