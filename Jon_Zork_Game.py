1dc905b370c1adb9ae99b40659edfe04bb61ae75
JSPAULD36 <45495373+JSPAULD36@users.noreply.github.com>
2018-12-04 14:26:07 -0600

    Kitchen room

---
+++ Jon_Zork_Game.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+
+playerItems = []
+enteredRooms = []
+openedDoors = []
+
+
+def kitchen():
+    global enteredRooms
+    global playerItems
+    exitRoom = False    
+    print("you have entered the Kitchen.")
+    print("There is a table with some items lying there.")
+    print("there is a door to the South and a door to the East.")
+    if kitchen in enteredRooms:
+        print('This room looks vaguely familiar')
+    if kitchen not in enteredRooms:
+        enteredRooms += ['room2']
+    table_items= ["candle", "key1", "rope"]
+    userAction = raw_input('What would you like to do? ')
+    while exitRoom == False:
+        if userAction == 'open East door':
+            print("The door is locked. Do you have a key that you could use?")
+            exitRoom = True
+        elif userAction == 'open South door':
+            print("you have gone to the backyard")
+            exitRoom = True
+        elif userAction == 'grab the items from the table':
+            print('you took', table_items)
+        else:
+            print('Invalid Action')
+            userAction = raw_input('What would you like to do? ')
+            
+            
+            
+def living_room():
+    global enteredRooms
+    global playerItems
+    exitRoom = False
+    print('You have entered the Living Room')
+    print('there is a door to the north.')
+    print('There is a dresser against the wall.')
+    print('There is a rug in the center of the room.')
+    print('There is a broken axe handle hanging on the wall.') 
+    if 'living_room' in enteredRooms:
+        enteredRooms =+ ['room3']
+        
+            
+        
+
