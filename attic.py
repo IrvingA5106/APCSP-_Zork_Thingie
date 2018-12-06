7138a9316e1d6c02f84aa2178ce89997e3f5753b
MggMuggins <mggmugginsmc@gmail.com>
2018-12-06 14:07:01 -0600

    Attic works

--- attic.py
+++ attic.py
@@ -1,32 +1,33 @@
+from kitchen import kitchen
 from player import Player
 
 def attic(player):
     wardrobe_open = False
-    attic_items = ['cobwebs', 'knife', 'skull']
+    attic_items = ['knife', 'skull']
     
     in_attic = True
-    if 'attic' in enteredRooms:
+    if 'attic' in player.visited_rooms:
         print("This room looks familiar.")
     else:
         # Good idea, Idk how to work something like this into player.add_room
         print("You open your eyes slowly. What happened?")
-        enteredRooms.append('attic')
+        player.visited_rooms.append('attic')
     
     print("You are in a dusty attic. The walls and ceiling are covered in cobwebs. A soaked wardrobe is on the side of the wall.")
-    print("There is a flight of stairs leading down to a doorway (door 5).")
+    print("There is a flight of stairs leading down to a doorway.")
     
     while in_attic:
-        userAction == raw_input("What do you want to do? ")
+        userAction = raw_input("What do you want to do? ")
         
-        if userAction == 'open door 5':
+        if userAction == 'open door':
             print('You walk down the stairs and open the door.')
             in_attic = False
-            #next room function yay!
+            kitchen(player)
         elif userAction == 'open wardrobe':
             print('The wardrobe is filled with waterlogged coats. All of them look ruined. At the bottom of the wardrobe is a box carved out of stone.')
-            wardrobeOpen == True
-        elif userAction == 'open box' and 'wardrobeOpen' == True:
-            print("You open the box. Inside, there is a silver knife and a dusty skull.You take both.")
+            wardrobe_open = True
+        elif userAction == 'open box' and wardrobe_open == True:
+            print("You open the box. Inside, there is a silver knife and a dusty skull. You take both.")
             player.pick_up(attic_items)
         else:
             print("I can't do that! Try again!")
