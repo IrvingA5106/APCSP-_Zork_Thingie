86f1d204b2b8ab5ca56fc9bddb985e680a574dc5
MggMuggins <mggmugginsmc@gmail.com>
2018-12-06 13:57:21 -0600

    Fix attic

--- attic.py
+++ attic.py
@@ -1,40 +1,35 @@
 from player import Player
 
-def attic():
-    global enteredRooms
-    global playerItems
-    global openedDoors
+def attic(player):
+    wardrobe_open = False
+    attic_items = ['cobwebs', 'knife', 'skull']
     
-    wardrobeOpen = False
-    atticItems = ['cobwebs', 'knife', 'skull']
-    
+    in_attic = True
     if 'attic' in enteredRooms:
         print("This room looks familiar.")
     else:
+        # Good idea, Idk how to work something like this into player.add_room
         print("You open your eyes slowly. What happened?")
         enteredRooms.append('attic')
-        
+    
     print("You are in a dusty attic. The walls and ceiling are covered in cobwebs. A soaked wardrobe is on the side of the wall.")
     print("There is a flight of stairs leading down to a doorway (door 5).")
-    userAction == raw_input("What do you want to do? ")
     
-    if userAction == 'open door 5':
-        print('You walk down the stairs and open the door.')
-        #next room function yay!
-    elif userAction == 'open wardrobe':
-        print('The wardrobe is filled with waterlogged coats. All of them look ruined. At the bottom of the wardrobe is a box carved out of stone.')
-        wardrobeOpen == True
-        userAction = raw_input("What would you like to do now? ")
-    elif userAction == 'open box' and 'wardrobeOpen' == True:
-        print("You open the box. Inside, there is a silver knife and a dusty skull.You take both.")
-        playerItems.append("knife")
-        playerItems.append("skull")
-        atticItems.remove("knife")
-        atticItems.remove("skull")
-        userAction = raw_input("What would you like to do now? ")
-    else:
-        print("I can't do that! Try again!")
-        userAction = raw_input("What would you like to do now? ")
+    while in_attic:
+        userAction == raw_input("What do you want to do? ")
         
+        if userAction == 'open door 5':
+            print('You walk down the stairs and open the door.')
+            in_attic = False
+            #next room function yay!
+        elif userAction == 'open wardrobe':
+            print('The wardrobe is filled with waterlogged coats. All of them look ruined. At the bottom of the wardrobe is a box carved out of stone.')
+            wardrobeOpen == True
+        elif userAction == 'open box' and 'wardrobeOpen' == True:
+            print("You open the box. Inside, there is a silver knife and a dusty skull.You take both.")
+            player.pick_up(attic_items)
+        else:
+            print("I can't do that! Try again!")
 
-    
+# For testing, run this file as main
+attic(Player())
