16dfab2c4b57c1a4ee69d6d2ef64d95626482cd8
MggMuggins <mggmugginsmc@gmail.com>
2018-12-10 14:37:19 -0600

    Probably fix living room

--- living_room.py
+++ living_room.py
@@ -5,7 +5,10 @@ from player import Player
 from foyer import foyer
 
 def living_room(player):
+    dresser_items = ['key2', 'key3','machete']
     exitRoom = False
+    rug_up = False
+    trapdoor_shut = True
     
     print('You have entered the Living Room')
     print('There is a door to the north.')
@@ -14,31 +17,32 @@ def living_room(player):
     
     player.add_room('living_room')
     
-    dresser_items = ['key2', 'key3','machete']
-    
     while exitRoom == False:
         userAction = raw_input('What would you like to do? ')
         
-        if userAction == 'open North door':
+        if userAction == 'open north door' and not player.has('key3'):
             print('This door is locked. Is there a key that you can use to open the door?')
-        elif userAction in ['open North door with key3', 'open North door with living room key']:
+        elif userAction == 'open north door' and player.has('key3'):
             print('you have opened the north door and entered the foyer')
             exitRoom = True
             foyer(player)
         elif userAction in ['move the rug', 'roll up the rug', 'move rug']:
             print('you have moved the rug')
             print('There is a trapdoor here that was covered by the rug')
-        elif userAction in ['open dresser drawer', 'pull the dresser drawer open', 'pull open drawer', 'pull open dresser drawer'] :
+            rug_up = True
+        elif userAction in ['open dresser', 'open dresser drawer', 'pull the dresser drawer open', 'pull open drawer', 'pull open dresser drawer'] :
             print('You have opened the dresser drawer.')
-            print('there is a key2, key3, and a machete in the drawer.')
+            print('there are two keys and a machete in the drawer.')
         elif userAction in ['take dresser items', 'grab items', 'take items', 'grab dresser items']:
-            print('You have grabbed the key2, key3, and the Machete.')
-        elif userAction in ['open trapdoor', ' enter trapdoor']:
+            print('You have grabbed the keys and the Machete.')
+            player.pick_up(dresser_items)
+        elif userAction in ['open trapdoor', ' enter trapdoor'] and (rug_up and trapdoor_shut):
             print('The trapdoor is tied closed by a thick string.')
-        elif userAction in ['cut the string' ' cut string with the machete']:
-            print(' You have cut the string and opened the trapdor.')
-        elif userAction in ['go into the trapdoor', ' enter trapdoor', 'exit trapdoor']:
-            print('you have entered the west end of the basement')
+        elif userAction in ['cut the string' ' cut string with the machete'] and player.has('machete'):
+            print('You have cut the string and opened the trapdor.')
+            trapdoor_shut = False
+        elif userAction in ['go into the trapdoor', ' enter trapdoor', 'exit trapdoor'] and (rug_up and not trapdoor_shut):
+            print('You have entered the west end of the basement')
             exitRoom = True
             basement(player)
         elif userAction == 'scream':
